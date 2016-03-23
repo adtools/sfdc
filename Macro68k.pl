eef0f1c6aca1ba3f11e2314f99fb6f1cb29efc9e
Jens Maus <mail@jens-maus.de>
2016-03-23 16:19:57 +0100

    implemented support for functions with multiple function pointer arguments. Previously only a single function pointer type was supported. Now corresponding LPXXXFPYY() macros will be used with YY being the number of function pointer arguments being used.

--- Macro68k.pl
+++ Macro68k.pl
@@ -28,17 +28,18 @@ BEGIN {
 	    my $a5        = $regs =~ /a5/;
 	    my $fp        = $argtypes =~ /\(\*+\)/;
       my $return    = $$prototype{'return'};
+      my $numfp     = 0;
 
 	    if ($a4 && $a5 && !$quiet) {
 		print STDERR "$$prototype{'funcname'} uses both a4 and a5 " .
 		    "for arguments. This is not going to work.\n";
 	    }
-	    
-	    $self->{FUNCARGTYPE} = '';
+	
+      @{$self->{FUNCARGTYPE}} = ();
 	    for my $argtype (@{$$prototype{'argtypes'}}) {
 		if ($argtype =~ /\(\*+\)/) {
-		    $self->{FUNCARGTYPE} = $argtype;
-		    last;
+		    @{$self->{FUNCARGTYPE}}[$numfp] = $argtype;
+        $numfp++;
 		}
 	    }
 
@@ -48,11 +49,12 @@ BEGIN {
         $self->{FUNCRETTYPE} = $return;
       }
 	    
-	    printf "	LP%d%s%s%s%s%s%s(0x%x, ", $$prototype{'numargs'},
+	    printf "	LP%d%s%s%s%s%s%s%s(0x%x, ", $$prototype{'numargs'},
 	    $prototype->{nr} ? "NR" : "",
 	    $prototype->{nb} ? "NB" : "",
 	    $a4 ? "A4" : "", $a5 ? "A5" : "",
-	    $self->{FUNCARGTYPE} ne '' ? "FP" : "",
+	    scalar @{$self->{FUNCARGTYPE}} > 0 ? "FP" : "",
+	    scalar @{$self->{FUNCARGTYPE}} > 1 ? scalar @{$self->{FUNCARGTYPE}} : "",
       $self->{FUNCRETTYPE} ne '' ? "FR" : "",
 	    $$prototype{'bias'};
 
@@ -81,16 +83,26 @@ BEGIN {
 	    my $argtype   = $params{'argtype'};
 	    my $argname   = $params{'argname'};
 	    my $argreg    = $params{'argreg'};
+      my $fpidx     = 0;
+      my $fpfound   = 0;
 	    
 	    if ($argreg eq 'a4' || $argreg eq 'a5') {
 		$argreg = 'd7';
 	    }
 	    
-	    if ($argtype =~ /\(\*+\)/) {
-		print ", __fpt, $argname, $argreg";
-	    }
-	    else {
-		print ", $argtype, $argname, $argreg";
+      for my $atype (@{$self->{FUNCARGTYPE}}) {
+        $fpidx++;
+        if ($atype eq $argtype) {
+          printf ", __fpt%s, %s, %s",
+            scalar @{$self->{FUNCARGTYPE}} > 1 ? $fpidx : "",
+            $argname, $argreg;
+          $fpfound = 1;
+          last;
+		    }
+      }
+
+	    if($fpfound eq 0) {
+        print ", $argtype, $argname, $argreg";
 	    }
 	}
         else {
@@ -103,19 +115,23 @@ BEGIN {
 	my %params    = @_;
 	my $prototype = $params{'prototype'};
 	my $sfd       = $self->{SFD};
+  my $fpidx     = 0;
 
 	if ($$prototype{'type'} eq 'function') {
 	    if (!$prototype->{nb}) {
 		print ",\\\n	, $self->{BASE}";
 	    }
 
-	    if ($self->{FUNCARGTYPE} ne '') {
-		my $fa = $self->{FUNCARGTYPE};
-
-		$fa =~ s/\((\*+)\)/($1__fpt)/;
-		
-		print ", $fa";
-	    }
+      for my $fa (@{$self->{FUNCARGTYPE}}) {
+        $fpidx++;
+        if(scalar @{$self->{FUNCARGTYPE}} gt 1) {
+		      $fa =~ s/\((\*+)\)/($1__fpt$fpidx)/;
+        }
+        else {
+		      $fa =~ s/\((\*+)\)/($1__fpt)/;
+        }
+		    print ", $fa";
+      }
 
       if ($self->{FUNCRETTYPE} ne '')
       {
