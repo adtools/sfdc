c12426894dcf1a0d134c0224cb224900e453d176
Jens Maus <mail@jens-maus.de>
2016-03-29 23:23:00 +0200

    fixed generation of MOS macros to actually use the right LP() type inline macros.

--- MacroMOS.pl
+++ MacroMOS.pl
@@ -4,7 +4,7 @@
 BEGIN {
     package MacroMOS;
     use vars qw(@ISA);
-    @ISA = qw (MacroLP);
+    @ISA = qw (Macro68k);
 
     sub new {
 	my $proto  = shift;
@@ -14,20 +14,120 @@ BEGIN {
 	return $self;
     }
 
-    sub function_end {
+    sub header {
+	my $self = shift;
+	my $sfd  = $self->{SFD};
+
+	print "/* Automatically generated header (sfdc SFDC_VERSION)! Do not edit! */\n";
+	print "\n";
+	print "#ifndef _PPCINLINE_$$sfd{'BASENAME'}_H\n";
+	print "#define _PPCINLINE_$$sfd{'BASENAME'}_H\n";
+	print "\n";
+	
+	print "#ifndef __PPCINLINE_MACROS_H\n";
+	print "#include <ppcinline/macros.h>\n";
+	print "#endif /* !__PPCINLINE_MACROS_H */\n";
+	print "\n";
+
+	if ($$sfd{'base'} ne '') {
+	    print "#ifndef $self->{BASE}\n";
+	    print "#define $self->{BASE} $$sfd{'base'}\n";
+	    print "#endif /* !$self->{BASE} */\n";
+	    print "\n";
+	}
+    }
+
+    sub footer {
+	my $self = shift;
+	my $sfd  = $self->{SFD};
+
+	print "#endif /* !_PPCINLINE_$$sfd{'BASENAME'}_H */\n";
+    }
+
+    sub function_start {
 	my $self      = shift;
 	my %params    = @_;
 	my $prototype = $params{'prototype'};
 	my $sfd       = $self->{SFD};
 
-	
 	if ($$prototype{'type'} eq 'function') {
 
+	    my $regs      = join(',', @{$$prototype{'regs'}});
+	    my $argtypes  = join(',', @{$$prototype{'argtypes'}});
+	    my $fp        = $argtypes =~ /\(\*+\)/;
+      my $return    = $$prototype{'return'};
+      my $numfp     = 0;
+
+      @{$self->{FUNCARGTYPE}} = ();
+	    for my $argtype (@{$$prototype{'argtypes'}}) {
+		if ($argtype =~ /\(\*+\)/) {
+		    @{$self->{FUNCARGTYPE}}[$numfp] = $argtype;
+        $numfp++;
+		}
+	    }
+
+      $self->{FUNCRETTYPE} = '';
+      if($return =~ /\(\*+\)/)
+      {
+        $self->{FUNCRETTYPE} = $return;
+      }
+	    
+	    printf "	LP%d%s%s%s%s%s(0x%x, ", $$prototype{'numargs'},
+	    $prototype->{nr} ? "NR" : "",
+	    $prototype->{nb} ? "NB" : "",
+	    scalar @{$self->{FUNCARGTYPE}} > 0 ? "FP" : "",
+	    scalar @{$self->{FUNCARGTYPE}} > 1 ? scalar @{$self->{FUNCARGTYPE}} : "",
+      $self->{FUNCRETTYPE} ne '' ? "FR" : "",
+	    $$prototype{'bias'};
+
+      if ($self->{FUNCRETTYPE})
+      {
+        print "__fpr, "; 
+      }
+	    elsif (!$prototype->{nr}) {
+		print "$$prototype{'return'}, ";
+	    }
+
+	    print "$$prototype{'funcname'} ";
+	}
+	else {
+	    $self->SUPER::function_start (@_);
+	}
+    }
+
+    sub function_end {
+	my $self      = shift;
+	my %params    = @_;
+	my $prototype = $params{'prototype'};
+	my $sfd       = $self->{SFD};
+  my $fpidx     = 0;
+
+	if ($$prototype{'type'} eq 'function') {
 	    if (!$prototype->{nb}) {
 		print ",\\\n	, $self->{BASE}";
 	    }
 
-	    print ", IF_CACHEFLUSHALL, NULL, 0, IF_CACHEFLUSHALL, NULL, 0)\n";
+      for my $fa (@{$self->{FUNCARGTYPE}}) {
+        $fpidx++;
+        if(scalar @{$self->{FUNCARGTYPE}} gt 1) {
+		      $fa =~ s/\((\*+)\)/($1__fpt$fpidx)/;
+        }
+        else {
+		      $fa =~ s/\((\*+)\)/($1__fpt)/;
+        }
+		    print ", $fa";
+      }
+
+      if ($self->{FUNCRETTYPE} ne '')
+      {
+        my $fr = $self->{FUNCRETTYPE};
+
+        $fr =~ s/\((\*+)\)/($1__fpr)/;
+
+        print ", $fr";
+      }
+	    
+	    print ", 0, 0, 0, 0, 0, 0)\n";
 	}
 	else {
 	    $self->SUPER::function_end (@_);
